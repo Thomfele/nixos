ef8fbb0efb169642dd2a93c704b5d6d1bc423d51
florisbechger <69296716+florisbechger@users.noreply.github.com>
2026-04-29 17:52:32 +0000

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -5,36 +5,29 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./modules/alsa.nix
-      ./modules/applications.nix
-      ./modules/bluetooth.nix
 
-      ./modules/boot-esp.nix
+      ./modules/boot.esp.nix
+      ./modules/ssh.disable.nix
+      ./modules/users.nix
+
+      ./modules/hostname.nix
+      ./modules/system.nix
+      ./modules/laptop.dhcp.nix
+      ./modules/services.nix
+
+      ./modules/alsa.nix
+      ./modules/bluetooth.nix
       ./modules/clamav.nix
       ./modules/firewall.nix
+      ./modules/print.nix
+
+      ./modules/applications.nix
+      ./modules/packages.nix
       ./modules/flakes.nix
       ./modules/flatpak.nix
       ./modules/gnome.nix
       ./modules/kvm.nix
 
-      ./modules/laptop.dhcp.nix
-      ./modules/laptop.nix
-      ./modules/packages.nix
-      ./modules/print.nix
-      ./modules/services.nix
-
-      ./misc/p43s.nix
-      ./misc/mega.nix
     ];
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.floris = {
-    isNormalUser = true;
-    description = "Floris";
-    extraGroups = [ "audio" "networkmanager" "wheel" "libvirtd" ];
-    packages = with pkgs; [
-    # some package
-    ];
-  };
 
 }
