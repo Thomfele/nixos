071f5db5335137f3691da3155c19c5ce8f9fdd9a
florisbechger <69296716+florisbechger@users.noreply.github.com>
2026-05-01 08:40:24 +0000

    Add files via upload

--- flakes/modules/orchestrate.nix
+++ flakes/modules/orchestrate.nix
@@ -1,4 +1,4 @@
-# Example 1
+# Example
 
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -19,46 +19,4 @@
         };
       };
     };
-}
-
-# Example 2
-
-{
-  description = "NixOS configurations for multiple hosts";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-  in
-  {
-    nixosConfigurations = {
-      host1 = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          {
-            networking = {
-              hostName = "host1";
-              ipAddress = "192.168.1.10";  # Set the IP address for host1
-            };
-          }
-        ];
-      };
-
-      host2 = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          {
-            networking = {
-              hostName = "host2";
-              ipAddress = "192.168.1.20";  # Set the IP address for host2
-            };
-          }
-        ];
-      };
-    };
-  };
 }
