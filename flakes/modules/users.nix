df5a4a3ee9d1941626b2871375fbac96e2c447be
Thomfele <69296716+Thomfele@users.noreply.github.com>
2026-05-04 16:20:05 +0000

    Add files via upload

--- flakes/modules/users.nix
+++ flakes/modules/users.nix
@@ -8,33 +8,40 @@
 
 {
 
+  # Create groups
+  users.groups.${admin} = {};
+  users.groups.${service} = {};
+
+  # Create user
   users.users.${admin} = {
     isNormalUser = true;
     description = "${admin}";
-    group = "users";
+    group = "${admin}";
     extraGroups = [ "wheel" "networkmanager" "audio" "libvirtd" "podman" ];
     initialPassword = "passwd";
     home  = "/home/${admin}";
-    createHome = true;
+    createHome = false;
     shell = "/run/current-system/sw/bin/bash";
     #packages = with pkgs; [
     # some package
     #];
   };
 
+  # Create user
   users.users.${service} = {
-    isNormalUser = true;
+    isSystemUser = true;
     description = "${service}";
-    group = "users";
-    #extraGroups = [ "wheel" "networkmanager" "audio" ];
-    createHome = false;
+    group = "${service}";
+    shell = "/usr/bin/nologin"; # No shell
+    createHome = false; # No homedirectory
+    password = "!"; # No password
     #packages = with pkgs; [
     # some package
     #];
   };
 
   # Allow the user to log in as root with a password.
-  users.users.root.initialPassword = "passwd";
+  users.users.root.initialPassword = "passwd"; # Change after deploy
 
   # Don't require sudo/root to `reboot` or `poweroff`.
   security.polkit.enable = true;
